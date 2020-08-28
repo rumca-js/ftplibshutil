13ef9eca3bbf79f3862f119e3e4f36a5e7f8c422
Piotr Zielinski <rozbujnik@gmail.com>
2020-08-28 22:01:51 +0200

    update example

--- examples/down.py
+++ examples/down.py
@@ -25,9 +25,32 @@ def read_args():
     parser = argparse.ArgumentParser(description='Download upload manager')
     parser.add_argument('-d','--download', dest='download', action="store_true", help='Download operation')
     parser.add_argument('-u', '--upload', dest='upload', action="store_true",  help='Upload operation)')
+    parser.add_argument('-t', '--test', dest='test', action="store_true",  help='Test operation)')
 
     args = parser.parse_args()
     return parser, args
+
+
+def download_dir_clean(ftp, remote_dir, local_dir):
+    if os.path.isdir(local_dir):
+        shutil.rmtree(local_dir)
+    ftp.downloadtree(remote_dir, ".")
+
+
+def upload_dir_clean(ftp, local_dir, remote_dir):
+    if ftp.exists(remote_dir):
+        ftp.rmtree(remote_dir)
+    ftp.uploadtree(local_dir, "/")
+
+
+def swap_dirs(ftp, from_dir, to_dir):
+    if ftp.exists(to_dir):
+        ftp.rename(to_dir, "/tmp")
+
+    ftp.rename(from_dir, to_dir)
+
+    if ftp.exists("/tmp"):
+        ftp.rmtree("/tmp")
 
 
 def main():
@@ -35,15 +58,43 @@ def main():
 
     if args.download:
         ftp = connect()
-        ftp.downloadtree("/Documents", ".")
+
+        download_dir_clean(ftp, "/Documents", "./Documents")
+        download_dir_clean(ftp, "/Backup", "./Backup")
+        download_dir_clean(ftp, "/dyi-page", "./dyi-page")
+
+        if os.path.isdir("./Backup"):
+            shutil.rmtree("./Backup")
         ftp.downloadtree("/Backup", ".")
+
+        if os.path.isdir("./dyi-page"):
+            shutil.rmtree("./dyi-page")
         ftp.downloadtree("/dyi-page", ".")
+
+        ftp.downloadfile("/index.html", "./index.html")
+        ftp.downloadfile("/.htaccess", "./.htaccess")
+
         ftp.quit()
+
     elif args.upload:
         ftp = connect()
-        ftp.uploadtree("Documents", "/")
-        ftp.uploadtree("Backup", "/")
-        ftp.uploadtree("dyi-page", "/")
+
+        upload_dir_clean(ftp, "Documents", "/Documents")
+        upload_dir_clean(ftp, "Backup", "/Backup")
+        upload_dir_clean(ftp, "dyi-page", "/dyi-page")
+        upload_dir_clean(ftp, "dyi-page/blog-html", "/blog-html")
+
+        swap_dirs(ftp, "/blog-html", "/blog")
+
+        ftp.uploadfile("index.html", "/index.html")
+        ftp.uploadfile(".htaccess", "/.htaccess")
+
+        ftp.quit()
+    elif args.test:
+        ftp = connect()
+
+        print( ftp.read_file("index.html") )
+
         ftp.quit()
     else:
         parser.print_help()
